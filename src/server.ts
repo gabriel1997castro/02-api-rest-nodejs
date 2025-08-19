f090ca6ee8b6ee8a0b3d002f707dd54772f72da8
Gabriel Castro <gabriel1997.castro@gmail.com>
2025-08-19 17:46:02 -0300

    Create transaction route

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-// http://localhost:3000/hello
-app.get('/hello', async () => {
-  const transactions = await knex('transactions').select('*')
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: '/transactions',
 })
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log('Server is running on http://localhost:3333')
+  console.log(`Server is running on http://localhost:${env.PORT}`)
 })

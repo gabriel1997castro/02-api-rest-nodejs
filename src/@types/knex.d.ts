000b71b82fcb49a5e3609faf6963c30ee32bd85c
Gabriel Castro <gabriel1997.castro@gmail.com>
2025-08-19 17:52:34 -0300

    Add tables types to Knex

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Knex } from 'knex'
+
+// integrate knex with typescript
+declare module 'knex/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      created_at: string
+      session_id?: string
+    }
+  }
+}
